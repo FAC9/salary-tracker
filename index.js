55991c0c6db524bfc9c8b48017cda46d5abf2463
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2018-04-22 08:20:31 +0100

    Updates port to PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./src/server');
-let port = process.env.port || 5000;
+let port = process.env.PORT || 5000;
 
 http.createServer(app).listen(port, () => {
   console.log(`Server is running on port: ${port}`);
