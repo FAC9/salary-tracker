dfd46a673f4c46d70e9446dcf9057259b8ad02f5
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2018-04-22 08:06:21 +0100

    Updates index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./src/server');
-let port = process.env.port || 4000;
+let port = process.env.port || 5000;
 
 http.createServer(app).listen(port, () => {
-  console.log(`Server is running on port: 4000`);
+  console.log(`Server is running on port: ${port}`);
 });
